9a3335e817e88a2a04125fd7ca5fa0df540a411c
Konstantin Bukin <bukin@inetstudio.ru>
2019-03-15 04:05:34 +0300

    Fix captcha validation

--- src/Http/Controllers/Front/CommentsController.php
+++ src/Http/Controllers/Front/CommentsController.php
@@ -4,8 +4,9 @@ namespace InetStudio\Comments\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Arcanedev\NoCaptcha\Rules\CaptchaRule;
 use InetStudio\Comments\Contracts\Services\Front\CommentsServiceContract;
-use InetStudio\Comments\Contracts\Http\Requests\Front\SendCommentRequestContract;
 use InetStudio\Comments\Contracts\Http\Responses\Front\GetCommentsResponseContract;
 use InetStudio\Comments\Contracts\Http\Responses\Front\SendCommentResponseContract;
 use InetStudio\Comments\Contracts\Http\Controllers\Front\CommentsControllerContract;
@@ -19,17 +20,43 @@ class CommentsController extends Controller implements CommentsControllerContrac
      * Отправка комментария.
      *
      * @param CommentsServiceContract $commentsService
-     * @param SendCommentRequestContract $request
+     * @param Request $request
      * @param string $type
      * @param string $id
      *
      * @return SendCommentResponseContract
      */
     public function sendComment(CommentsServiceContract $commentsService,
-                                SendCommentRequestContract $request,
+                                Request $request,
                                 string $type,
                                 string $id): SendCommentResponseContract
     {
+        $rules = [
+            'message' => 'required',
+        ];
+
+        if (! auth()->user()) {
+            $rules = array_merge($rules, [
+                'name' => 'required|max:255',
+                'email' => 'required|max:255|email',
+                'g-recaptcha-response' => [
+                    'required',
+                    new CaptchaRule,
+                ],
+            ]);
+        }
+
+        Validator::make($request->all(), $rules, [
+            'message.required' => 'Поле «Сообщение» обязательно для заполнения',
+            'name.required' => 'Поле «Имя» обязательно для заполнения',
+            'name.max' => 'Поле «Имя» не должно превышать 255 символов',
+            'email.required' => 'Поле «Email» обязательно для заполнения',
+            'email.max' => 'Поле «Email» не должно превышать 255 символов',
+            'email.email' => 'Поле «Email» должно содержать значение в корректном формате',
+            'g-recaptcha-response.required' => 'Поле «Капча» обязательно для заполнения',
+            'g-recaptcha-response.captcha'  => 'Неверный код капча',
+        ])->validate();
+
         $data = $request->only($commentsService->model->getFillable());
 
         $item = $commentsService->saveComment($data, $type, $id);
